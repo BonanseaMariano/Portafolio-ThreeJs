b327a3460d00f57b2b1251876efd8398f5c73dd4
Bonansea Mariano <marianobonanseapetrovial@gmail.com>
2025-03-10 00:21:31 -0300

    Traducciones y nuevo Globe

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react';
-import Globe from 'react-globe.gl';
-
 import Button from '../components/Button.jsx';
+import DayNightGlobe from '../components/DayNightGlobe.jsx';
 
 const About = () => {
   const [hasCopied, setHasCopied] = useState(false);
 
   const handleCopy = () => {
-    navigator.clipboard.writeText(' adrian@jsmastery.pro');
+    navigator.clipboard.writeText('marianobonanseapetrovial@gmail.com');
     setHasCopied(true);
 
     setTimeout(() => {
@@ -23,10 +22,10 @@ const About = () => {
             <img src="assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
 
             <div>
-              <p className="grid-headtext">Hi, I’m Adrian Hajdin</p>
+              <p className="grid-headtext">Hola, soy Mariano Bonansea</p>
               <p className="grid-subtext">
-                With 12 years of experience, I have honed my skills in both frontend and backend dev, creating dynamic
-                and responsive websites.
+                Soy estudiante de Licenciatura en Informática y me apasiona el desarrollo web. Estoy en constante
+                aprendizaje y busco siempre mejorar mis habilidades.
               </p>
             </div>
           </div>
@@ -39,8 +38,8 @@ const About = () => {
             <div>
               <p className="grid-headtext">Tech Stack</p>
               <p className="grid-subtext">
-                I specialize in a variety of languages, frameworks, and tools that allow me to build robust and scalable
-                applications
+                Me especializo en una variedad de lenguajes, frameworks y herramientas que me permiten construir
+                aplicaciones robustas y escalables.
               </p>
             </div>
           </div>
@@ -48,23 +47,15 @@ const About = () => {
 
         <div className="col-span-1 xl:row-span-4">
           <div className="grid-container">
-            <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
-              <Globe
-                height={326}
-                width={326}
-                backgroundColor="rgba(0, 0, 0, 0)"
-                backgroundImageOpacity={0.5}
-                showAtmosphere
-                showGraticules
-                globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-                labelsData={[{ lat: 40, lng: -100, text: 'Rjieka, Croatia', color: 'white', size: 15 }]}
-              />
+            <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center" style={{ overflow: 'hidden' }}>
+              <DayNightGlobe />
             </div>
             <div>
-              <p className="grid-headtext">I’m very flexible with time zone communications & locations</p>
-              <p className="grid-subtext">I&apos;m based in Rjieka, Croatia and open to remote work worldwide.</p>
-              <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+              <p className="grid-headtext">Flexible con zonas horarias y ubicaciones</p>
+              <p className="grid-subtext">Vivo en Puerto Madryn, Argentina y me encuentro abierto a trabajar de forma remota en todo el mundo.</p>
+              <a href="#contact" className="w-fit">
+                <Button name="Contáctame" isBeam containerClass="w-full mt-10" />
+              </a>
             </div>
           </div>
         </div>
@@ -74,10 +65,9 @@ const About = () => {
             <img src="assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain" />
 
             <div>
-              <p className="grid-headtext">My Passion for Coding</p>
+              <p className="grid-headtext">Mi Pasión por la Programación</p>
               <p className="grid-subtext">
-                I love solving problems and building things through code. Programming isn&apos;t just my
-                profession—it&apos;s my passion. I enjoy exploring new technologies, and enhancing my skills.
+                Me encanta resolver problemas y construir cosas a través del código. La programación no es solo mi profesión, es mi pasión. Disfruto explorando nuevas tecnologías y mejorando mis habilidades.
               </p>
             </div>
           </div>
@@ -92,10 +82,10 @@ const About = () => {
             />
 
             <div className="space-y-2">
-              <p className="grid-subtext text-center">Contact me</p>
+              <p className="grid-subtext text-center">Contáctame</p>
               <div className="copy-container" onClick={handleCopy}>
                 <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy" />
-                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">adrian@jsmastery.pro</p>
+                <p className="md:text-sm text-lg  font-medium text-gray_gradient text-white">marianobonanseapetrovial@gmail.com</p>
               </div>
             </div>
           </div>
